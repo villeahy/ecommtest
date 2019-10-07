4178ea5a1fa84ca998855d598858d10132753f00
Hytönen <ville.hytonen@cgi.com>
2019-10-07 13:21:22 +0300

    added plugins for stripe

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
+require("dotenv")
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Testing strip ecomm`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@Pena`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,6 +27,15 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    "gatsby-plugin-stripe",
+    {
+      resolve: `gatsby-source-stripe`,
+      options: {
+        objects: ["Sku"],
+        secretKey: process.env.STRIPE_SECRET_KEY,
+        downloadFiles: true,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
